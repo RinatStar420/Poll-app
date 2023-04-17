5ce1b21a7bcd2176691800d2ba0518245f8613c1
rinatrinat <rinatluske@gmail.com>
2023-04-17 14:07:11 +0300

    init commit

---
+++ polls/api/serializers.py
@@ -0,0 +1,43 @@
+from ..models import Poll, Choice, Vote
+from django.contrib.auth.models import User
+from rest_framework import serializers  # сократит дублирование кода за счет автоматического определения набора
+from rest_framework.authtoken.models import Token
+
+
+# полей и создания реализаций методов create() и update().
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = '__all__'
+
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    votes = VoteSerializer(many=True, required=False)
+
+    class Meta:
+        model = Choice
+        fields = '__all__'
+
+
+class PollSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True, read_only=True, required=False)
+
+    class Meta:
+        model = Poll
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}} # получать пароль обратно в ответ
+
+    def create(self, validated_data):
+        user = User(email=validated_data['email'], username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        Token.objects.create(user=user)
+        return user
